5aa2d49b084699575cabb8922e7164410b5ccc87
alain-mathy <alain_mathy@hotmail.com>
2025-08-28 16:33:58 +0200

    Add Student entity and AppDbContext

---
+++ MVC_Crud_Net8/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Crud_Net8.Models.Entities;
+
+namespace MVC_Crud_Net8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ MVC_Crud_Net8/Models/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Crud_Net8.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
