a5cbf6c75e4327a29289c2c833d4b7a81ebbf457
alain-mathy <alain_mathy@hotmail.com>
2025-08-29 08:52:02 +0200

    Add Edit and Delete -> StudentController

--- MVC_Crud_Net8/Controllers/StudentController.cs
+++ MVC_Crud_Net8/Controllers/StudentController.cs
@@ -34,8 +34,7 @@ namespace MVC_Crud_Net8.Controllers
 
             await _context.Students.AddAsync(student);
             await _context.SaveChangesAsync();
-
-            return View();
+            return RedirectToAction("GetAll");
         }
 
         [HttpGet]
@@ -43,6 +42,45 @@ namespace MVC_Crud_Net8.Controllers
         {
             var students = await _context.Students.ToListAsync();
             return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var result = await _context.Students.FindAsync(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student model)
+        {
+            var student = await _context.Students.FindAsync(model.Id);
+            if (student is not null)
+            {
+                student.Name = model.Name;
+                student.Email = model.Email;
+                student.Phone = model.Phone;
+                student.Subscribed = model.Subscribed;
+                await _context.SaveChangesAsync();
+                return RedirectToAction("GetAll");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student model)
+        {
+            var student = await _context.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.Id == model.Id);
+            if (student is not null)
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("GetAll");
         }
     }
 }
